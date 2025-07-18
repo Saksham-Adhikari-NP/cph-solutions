75b7c841b917df1857a024698401d1b98e59ce4e
Saksham-Adhikari-NP <saksham71032@gmail.com~>
2025-07-18 17:36:10 +0545

    Auto-commit: Updated autopush.sh

--- autopush.sh
+++ autopush.sh
@@ -5,8 +5,11 @@ cd "$WATCH_DIR"
 
 inotifywait -m -e close_write --format '%w%f' . | while read FILE
 do
-    echo "Detected change in $FILE"
-    git add "$FILE"
-    git commit -m "Auto-commit: Updated $(basename "$FILE")"
-    git push origin main
+    if [[ "$FILE" == *.cpp ]]; then
+        echo "Detected C++ file change: $FILE"
+        git add "$FILE"
+        git commit -m "Auto-commit: Updated $(basename "$FILE")"
+        git push origin main
+    fi
 done
+
