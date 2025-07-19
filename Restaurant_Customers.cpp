e18f5835f91e8c68634452f63209f7135aaef396
Saksham-Adhikari-NP <saksham71032@gmail.com~>
2025-07-20 04:37:55 +0545

    Auto-commit: Updated Restaurant_Customers.cpp

---
+++ Restaurant_Customers.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std; 
+bool cmp(int<pair<int,int>>a, int<pair<int,int>>b)
+{
+    return a.second<b.second;
+}
+int main() {
+
+    int n;
+    vector<pair<int,int>>customer ;
+    for(int i=0;i<n;i++){
+        int a,b;
+        cin>>a>>b;
+        customer.push_back({a,b});
+    }
+
+    // Sort the customers based on their arrival time
+    sort(customer.begin(), customer.end(),cmp);
+
+vector<int> count(n,1);
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = i+1; j < n; j++) {
+            if (customer[j].first <= customer[i].second) {
+                count[i]++;
+            }
+        }
+    }
+max = *max_element(count.begin(), count.end());
+    cout << max ;
+    
+    return 0;
+}
