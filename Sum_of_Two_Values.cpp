d1ad6cccb2cea537639fd05bfcf573c2b60e2b0e
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-09-21 16:53:05 +0545

    Auto-commit: Updated Sum_of_Two_Values.cpp

--- Sum_of_Two_Values.cpp
+++ Sum_of_Two_Values.cpp
@@ -3,15 +3,15 @@ using namespace std;
 int main (){
 
     int n;
-    long long x;
+    int x;
     cin >> n >> x;
     vector<long long> a(n);
     for (int i = 0; i < n; i++) cin >> a[i];
 
-    unordered_map<long long, int> mp; // value -> index (1-based)
+    unordered_map<int, int> mp; // value -> index (1-based)
 
     for (int i = 0; i < n; i++) {
-        long long need = x - a[i];
+        int need = x - a[i];
         if (mp.count(need)) {
             cout << mp[need] << " " << i+1 << "\n";
             return 0;
