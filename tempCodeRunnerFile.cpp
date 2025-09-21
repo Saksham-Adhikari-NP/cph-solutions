4c93f3dd033a280eb6d3b4f9addc3b140ae76be3
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-09-21 15:49:57 +0545

    Auto-commit: Updated tempCodeRunnerFile.cpp

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+     
+    map<char , int> mp;
+    map['a'] = 1;
+    map['b'] = 2;
+    map['c'] = 3;
+     cout<< m.begin()->first << " " << m.begin()->second << endl;
+     m.erase(2);
+     cout<< m.size() << endl;
+        for(auto it : m){
+            cout<< it.first << " " << it.second << endl;
+        }
+    return 0;
+    
+    
+    }
