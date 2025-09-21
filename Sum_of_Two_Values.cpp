04d9b501cd8f62e8eb6cbb9d37f98e18b7aa18d1
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-09-21 16:50:07 +0545

    Auto-commit: Updated Sum_of_Two_Values.cpp

--- Sum_of_Two_Values.cpp
+++ Sum_of_Two_Values.cpp
@@ -1,31 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main (){
-int n,x,sum=0;
-bool found = false;
-cin >> n >> x;
-vector<int> a(n);
-for (int i=0;i<n;i++){
-    cin >> a[i];
-}
-int l=0 ,r =0;
-while( r<n ) 
-{ 
-sum += a[r];
-while( sum > x  && l<=r ) 
-{
-sum -= a[l];
-l++;          
-}
-if( sum == x ) 
-{
-    found = true;
-cout << l+1 << " " << r+1 << endl;
-}
-r++ ;
-}
-if (!found)
-{
-cout << "IMPOSSIBLE" << endl;
-}
+
+    int n;
+    long long x;
+    cin >> n >> x;
+    vector<long long> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    unordered_map<long long, int> mp; // value -> index (1-based)
+
+    for (int i = 0; i < n; i++) {
+        long long need = x - a[i];
+        if (mp.count(need)) {
+            cout << mp[need] << " " << i+1 << "\n";
+            return 0;
+        }
+        mp[a[i]] = i+1;
+    }
+
+    cout << "IMPOSSIBLE\n";
 }
