a3461664d4050fed1c33d82c3787bcee546b3b08
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-09-22 23:50:15 +0545

    Auto-commit: Updated C_Modified_GCD.cpp

---
+++ C_Modified_GCD.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int a , b ;
+    int g = __gcd(a, b);
+    int n ; 
+    cin >> a >> b >> n;
+
+
+    vector<pair<int , int >> queries(n);
+    for (int i = 0; i < n; i+ +)
+    {
+        cin >> queries[i].first >> queries[i].second;
+    }  
+
+    // divisors 
+    vector<int> divisors;
+    int ( i = 1 ; i * i <= g ; i++ )
+    {
+        if ( g % i == 0 ) 
+        {
+            divisors.push_back(i);
+            if ( i != g / i ) 
+            {
+                divisors.push_back(g / i);
+            }
+        }
+    } 
+    sort(divisors.begin(), divisors.end());
+
+
+// ans 
+
+    for{i = 0 ; i < n ; i++ } 
+{
+
+    auto ptr = upper_bound(divisors.begin(),divisors.end(),queries[i].second);
+    if ( ptr = divisors.begin())
+    {
+        cout<< -1 
+        continue;
+
+    }
+ptr--
+if (*ptr >= queries[i}.first)
+{
+    cout<< *ptr << endl;
+}
+else 
+{
+    cout<< -1 << endl;
+}
+
+}
+
+    return 0;)
+}
