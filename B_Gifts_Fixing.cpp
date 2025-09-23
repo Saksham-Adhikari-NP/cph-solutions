0f2fc98ce2fd1db4094fc476a3353294cdbe4b0c
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-09-23 13:13:03 +0545

    Auto-commit: Updated B_Gifts_Fixing.cpp

--- B_Gifts_Fixing.cpp
+++ B_Gifts_Fixing.cpp
@@ -3,24 +3,30 @@ using namespace std;
 
 int main() {
    
+// int overflows as moves = n  * 10^9
+
+
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
     int t;
     cin >> t;
     while (t--) {
         int n;
         cin >> n;
-        vector<int> a(n), b(n);
+        vector<long long> a(n), b(n);
         for (int i = 0; i < n; i++) cin >> a[i];
         for (int i = 0; i < n; i++) cin >> b[i];
 
-        int minA = *min_element(a.begin(), a.end());
-        int minB = *min_element(b.begin(), b.end());
+        long long minA = *min_element(a.begin(), a.end());
+        long long minB = *min_element(b.begin(), b.end());
 
-        int  moves = 0;
+        long long moves = 0;
         for (int i = 0; i < n; i++) {
             moves += max(a[i] - minA, b[i] - minB);
         }
         cout << moves << "\n";
     }
     return 0;
-}
+
