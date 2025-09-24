63df3fd9506d150f72cc138bbeb098c517c3e159
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-09-24 17:24:14 +0545

    Auto-commit: Updated C_Move_Brackets.cpp

---
+++ C_Move_Brackets.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () 
+{
+     int t; 
+     cin>> t ;
+      while (t --)
+      {
+         int n; 
+        string s;
+        cin >> n >> s; 
+        int count = 0 , , result = 0 ; 
+
+        for (int i = 0 ; i < n ; i++ )
+        {
+            if ( s[i] == (')')
+            {
+                count--;
+
+            }
+            else count --; 
+
+            result = min(result , count);
+
+        } 
+
+        cout << abs(result) << "\n";
+}
