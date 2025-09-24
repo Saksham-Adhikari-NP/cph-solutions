8edba9cca492f78449b52cb59b7649cbe2049d6e
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-09-24 14:09:09 +0545

    Auto-commit: Updated A_Short_Substrings.cpp

---
+++ A_Short_Substrings.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main () 
+_{
+     int t;
+     cin >> t;
+    while(t--)
+    {
+        string b; 
+        cin >> b;
+        for(i=0 ; i < b.size(); i++)
+
+        {
+            if( i > 2 && i % 2 = 0)  
+            {
+                continue;
+            }
+            else {
+                cout << b[i];
+            }
+        }
+    }
+}
