c61904a7a9a7ff7abb009592dd911701f7b440c5
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-09-29 09:20:43 +0545

    Auto-commit: Updated C_Product_of_Three_Numbers.cpp

--- C_Product_of_Three_Numbers.cpp
+++ C_Product_of_Three_Numbers.cpp
@@ -13,7 +13,7 @@ int main ()
             int n , i ;
                 int a = 0 ,b=0 ,c = 0 ,d,m;
              cin >> n ; 
-            for ( i = 1 ; i*i <= n ; i++ )
+            for ( i = 2 ; i*i <= n ; i++ )
             { if (n%i == 0)
                 {
                      a = i ; 
