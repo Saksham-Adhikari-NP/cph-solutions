db44a66dcafb227bc6205057043e74a6e8471bb3
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-09-29 08:51:56 +0545

    Auto-commit: Updated C_Product_of_Three_Numbers.cpp

---
+++ C_Product_of_Three_Numbers.cpp
@@ -0,0 +1,42 @@
+#include<bits/STD++.h>
+using namespace std;
+int main () 
+{
+     int t,n ;
+    int a ,b=0 ,c; 
+    int i; 
+
+    cin >> t ;
+        while (t --)
+        [
+             cin >> n ; 
+            for ( i = 1 ; i*i <= n ; i++ )
+            { if (n%i != 0)
+                {
+                     b = i ; 
+                     break ; 
+                } 
+                
+            }   
+
+            if ( b == 0 )
+            {
+                 cout << NO << endl;
+
+            }
+            else { 
+
+                a*a = n 
+                c = n / ( a*b);
+                if ( (a>1_) && ( b >1 )) 
+                {
+                     cout << YES << endl;
+                        cout << a << " " << b << " " << c << endl;
+                }
+                else cout << NO << endl;
+            }
+
+
+        ]
+
+}
