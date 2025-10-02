c8bf3efde25a9ee31b5f526795bbd06b5f643a50
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-10-03 00:01:43 +0545

    Auto-commit: Updated Exponentiation.cpp

--- Exponentiation.cpp
+++ Exponentiation.cpp
@@ -1,19 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
-#define endl '\n'
 
-int32_t main() {
+const int MOD = 1e9 + 7;
+
+// Modular exponentiation
+long long modpow(long long a, long long b, long long m) {
+    if (b == 0) return 1;        // 0^0 = 1 also handled
+    long long res = 1;
+    a %= m;
+    while (b > 0) {
+        if (b % 2 == 1) res = (res * a) % m;
+        a = (a * a) % m;
+        b /= 2;
+    }
+    return res;
+}
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    int t;
-    cin >> t;
-    while (t--) {
-        // solve here
 
-
+    int n;
+    cin >> n;
+    while (n--) {
+        long long a, b;
+        cin >> a >> b;
+        cout << modpow(a, b, MOD) << "\n";
     }
-    
     return 0;
 }
