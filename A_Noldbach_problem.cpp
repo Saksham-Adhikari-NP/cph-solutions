0c22c9aaf9ba60d233c32aa4d492686b28085135
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-10-02 23:43:24 +0545

    Auto-commit: Updated A_Noldbach_problem.cpp

--- A_Noldbach_problem.cpp
+++ A_Noldbach_problem.cpp
@@ -6,14 +6,36 @@ using namespace std;
 int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    int t;
-    cin >> t;
-    while (t--) {
-        // solve here
+    int n , k ;
+    cin >> n >> k;
+     
+    bool is_prime[1001] = {false}:
+    for ( int i =2 ; i<= 1000 ; i++ ) is_prime[i] = true;
 
-
+    for (int i =2; i<= 1000 ; i++)
+    {
+        if(is_prime[i]) {
+             for (int j = i*i ; j<= n; j += i )
+             {
+                is_prime[j] = false; 
+            
+             }
+        }
     }
-    
+    vector<int> primes;
+    for (int i = 2; i <= n; i++) {
+        if (is_prime[i]) primes.push_back(i);
+    }
+
+
+    int count = 0;
+    for (int i = 0; i < primes.size() - 1; i++) {
+        int sum = primes[i] + primes[i+1] + 1;
+        if (sum <= n && is_prime[sum]) count++;
+    }
+
+    if (count >= k) cout << "YES\n";
+    else cout << endl;
+
     return 0;
 }
