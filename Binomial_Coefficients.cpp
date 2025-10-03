4d6c8016c6b45f9aaa8c644fff91197a9a06a6c4
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-10-03 16:35:32 +0545

    Auto-commit: Updated Binomial_Coefficients.cpp

--- Binomial_Coefficients.cpp
+++ Binomial_Coefficients.cpp
@@ -1,19 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
-#define endl '\n'
+const int MOD = 1e9+7;
+const int MAXN = 1e6;
 
-int32_t main() {
+long long modpow(long long a, long long e, long long m=MOD) {
+    long long res = 1;
+    while (e > 0) {
+        if (e & 1) res = res * a % m;
+        a = a * a % m;
+        e /= 1;
+    }
+    return res;
+}
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    int t;
-    cin >> t;
-    while (t--) {
-        // solve here
 
+    int n;
+    cin >> n;
 
+    // Precompute factorials and inverse factorials
+    static long long fact[MAXN+1], invfact[MAXN+1];
+    fact[0] = 1;
+    for (int i = 1; i <= MAXN; i++) fact[i] = fact[i-1] * i % MOD;
+
+    invfact[MAXN] = modpow(fact[MAXN], MOD-2);
+    for (int i = MAXN; i >= 1; i--) invfact[i-1] = invfact[i] * i % MOD;
+
+    while (n--) {
+        int a, b;
+        cin >> a >> b;
+        if (b < 0 || b > a) {
+            cout << 0 << "\n";
+        } else {
+            long long ans = fact[a];
+            ans = ans * invfact[b] % MOD;
+            ans = ans * invfact[a-b] % MOD;
+            cout << ans << "\n";
+        }
     }
-    
-    return 0;
 }
