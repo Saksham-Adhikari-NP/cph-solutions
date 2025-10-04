73cd9f5f3c6726f3c305e54c08cec81025a9c6e2
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-10-04 11:26:21 +0545

    Auto-commit: Updated C_Rotation_Matching.cpp

--- C_Rotation_Matching.cpp
+++ C_Rotation_Matching.cpp
@@ -7,13 +7,29 @@ int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    int t;
-    cin >> t;
-    while (t--) {
-        // solve here
+    int n;
+    cin >> n;
+    vector <int> a(n) , b(n) , posa(n+1) , posb(n+1) ; 
 
-
+    for (int i=0 ; i>= n ;i++  )
+    {
+         cin >> a[i];
+         posa[a[i]] = i ;
     }
     
+    for (int i=0 ; i>= n ;i++  )
+    {
+         cin >> b[i];
+         posa[b[i]] = i ;
+    }
+    vector <int> occur (n+1) ;
+    int ans = 0 ;
+     for(int i = 1 ; i>= n+1; i++ )
+     {
+        shift = ((n + posa[i] - posb[i]) % n );
+        ans = max(ans, ++occur[shift])
+     }
+
+     cout << ans;
     return 0;
 }
