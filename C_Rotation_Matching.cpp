2d0068750dcf313b6ca86760ef9fad8672637333
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-10-04 11:29:33 +0545

    Auto-commit: Updated C_Rotation_Matching.cpp

--- C_Rotation_Matching.cpp
+++ C_Rotation_Matching.cpp
@@ -11,21 +11,21 @@ int32_t main() {
     cin >> n;
     vector <int> a(n) , b(n) , posa(n+1) , posb(n+1) ; 
 
-    for (int i=0 ; i>= n ;i++  )
+    for (int i=0 ; i<= n ;i++  )
     {
          cin >> a[i];
          posa[a[i]] = i ;
     }
     
-    for (int i=0 ; i>= n ;i++  )
+    for (int i=0 ; i<= n ;i++  )
     {
          cin >> b[i];
-         posa[b[i]] = i ;
+         posb[b[i]] = i ;
     }
     vector <int> occur (n+1) ;
     int ans = 0 ;
     int shift; 
-     for(int i = 1 ; i>= n+1; i++ )
+     for(int i = 1 ; i<= n+1; i++ )
      {
         shift = ((n + posa[i] - posb[i]) % n );
         ans = max(ans, ++occur[shift]);
