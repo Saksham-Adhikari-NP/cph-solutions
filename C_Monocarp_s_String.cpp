fa9dc053b7cca3360a70becc61f85da15de4bd43
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-10-06 22:15:03 +0545

    Auto-commit: Updated C_Monocarp_s_String.cpp

--- C_Monocarp_s_String.cpp
+++ C_Monocarp_s_String.cpp
@@ -1,56 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve() {
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
     int t;
     cin >> t;
+    
     while (t--) {
         int n;
-        cin >> n;
         string s;
-        cin >> s;
-
-        int total_a = count(s.begin(), s.end(), 'a');
-        int total_b = count(s.begin(), s.end(), 'b');
-
+        cin >> n >> s;
+        
+        int total_a = 0, total_b = 0;
+        for (char c : s) {
+            if (c == 'a') total_a++;
+            else total_b++;
+        }
+        
         if (total_a == total_b) {
             cout << 0 << "\n";
             continue;
         }
-
+        
         if (total_a == 0 || total_b == 0) {
             cout << -1 << "\n";
             continue;
         }
-
+        
         int target_diff = total_a - total_b;
-        int min_length = n + 1;
-
+        int min_len = n + 1;
+        
         for (int i = 0; i < n; i++) {
-            int count_a = 0, count_b = 0;
+            int cnt_a = 0, cnt_b = 0;
             for (int j = i; j < n; j++) {
-                if (s[j] == 'a') count_a++;
-                else count_b++;
-
-                int diff = count_a - count_b;
+                if (s[j] == 'a') cnt_a++;
+                else cnt_b++;
+                
+                int diff = cnt_a - cnt_b;
+                
                 if (diff == target_diff) {
-                    int length = j - i + 1;
-                    int remaining_total = (total_a + total_b) - length;
-                    if (remaining_total % 2 == 0) {
-                        min_length = min(min_length, length);
-                    }
+                    int len = j - i + 1;
+                    int rem_a = total_a - cnt_a;
+                    int rem_b = total_b - cnt_b;
+                    if (rem_a == rem_b && rem_a > 0) min_len = min(min_len, len);
                 }
             }
         }
-
-        if (min_length == n + 1) cout << -1 << "\n";
-        else cout << min_length << "\n";
+        
+        if (min_len == n + 1) cout << -1 << "\n";
+        else cout << min_len << "\n";
     }
-}
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    solve();
+    
     return 0;
 }
