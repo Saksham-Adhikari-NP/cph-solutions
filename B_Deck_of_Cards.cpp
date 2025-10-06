2b6cc411702f9b8aeedca3eccd3a2fa3c65a07c8
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-10-06 21:29:04 +0545

    Auto-commit: Updated B_Deck_of_Cards.cpp

--- B_Deck_of_Cards.cpp
+++ B_Deck_of_Cards.cpp
@@ -1,19 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
-#define endl '\n'
 
-int32_t main() {
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
     int t;
     cin >> t;
     while (t--) {
-        // solve here
+        int n, k;
+        string s;
+        cin >> n >> k >> s;
 
+        int top = count(s.begin(), s.end(), '0');
+        int bottom = count(s.begin(), s.end(), '1');
+        int unknown = count(s.begin(), s.end(), '2');
 
+        int min_top = top, max_top = top + unknown;
+        int min_bottom = bottom, max_bottom = bottom + unknown;
+
+        string res(n, '+');
+        for (int i = 1; i <= n; i++) {
+            if (i <= min_top || i > n - min_bottom)
+                res[i-1] = '-';
+            else if (i <= max_top || i > n - max_bottom)
+                res[i-1] = '?';
+        }
+        cout << res << "\n";
     }
-    
-    return 0;
 }
