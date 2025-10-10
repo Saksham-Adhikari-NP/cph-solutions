66a34024bb7a54b9bc6010f948a91739d05935ae
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-10-10 22:30:07 +0545

    Auto-commit: Updated C_Symmetrical_Polygons.cpp

--- C_Symmetrical_Polygons.cpp
+++ C_Symmetrical_Polygons.cpp
@@ -3,61 +3,65 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
+    cin.tie(0);
     int t;
     cin >> t;
-    while (t--) {
+    for(int test = 0; test < t; test++) {
         int n;
         cin >> n;
         vector<long long> a(n);
-        for (int i = 0; i < n; i++) cin >> a[i];
-
-        sort(a.rbegin(), a.rend()); // descending
-        vector<long long> sides;
-        unordered_map<long long,int> freq;
-        for(auto x : a) freq[x]++;
-
-        // collect all pairs for symmetry
-        vector<long long> pairSides;
-        vector<long long> leftovers;
-        for(auto x : a) {
-            if(freq[x] >= 2) {
-                int pairs = freq[x]/2;
-                for(int i=0;i<pairs;i++){
-                    pairSides.push_back(x);
-                    pairSides.push_back(x);
-                }
-                freq[x] -= pairs*2;
+        for(auto &x : a) cin >> x;
+        sort(a.rbegin(), a.rend());
+        vector<pair<long long, int>> groups;
+        int i = 0;
+        while(i < n) {
+            long long len = a[i];
+            int cnt = 0;
+            while(i < n && a[i] == len) { cnt++; i++; }
+            groups.emplace_back(len, cnt);
+        }
+        int d = groups.size();
+        vector<long long> suffix_total(d + 1, 0);
+        vector<int> suffix_num(d + 1, 0);
+        for(int j = d - 1; j >= 0; j--) {
+            suffix_total[j] = suffix_total[j + 1] + groups[j].first * (long long)groups[j].second;
+            suffix_num[j] = suffix_num[j + 1] + groups[j].second;
+        }
+        vector<int> odd_indices;
+        for(int j = 0; j < d; j++) {
+            if(groups[j].second % 2 == 1) odd_indices.push_back(j);
+        }
+        int sz_odd = odd_indices.size();
+        vector<long long> odd_lengths(sz_odd);
+        for(int q = 0; q < sz_odd; q++) {
+            odd_lengths[q] = groups[odd_indices[q]].first;
+        }
+        vector<long long> suffix_odd(sz_odd + 1, 0);
+        for(int q = sz_odd - 1; q >= 0; q--) {
+            suffix_odd[q] = suffix_odd[q + 1] + odd_lengths[q];
+        }
+        long long ans = 0;
+        for(int start = 0; start < d; start++) {
+            long long S_full = suffix_total[start];
+            int m_full = suffix_num[start];
+            if(m_full < 3) continue;
+            long long MX = groups[start].first;
+            auto it = lower_bound(odd_indices.begin(), odd_indices.end(), start);
+            int p_odd = it - odd_indices.begin();
+            int cur_odd_n = sz_odd - p_odd;
+            long long rem = 0;
+            int numrem = 0;
+            if(cur_odd_n > 2) {
+                rem = suffix_odd[p_odd + 2];
+                numrem = cur_odd_n - 2;
+            }
+            long long this_S = S_full - rem;
+            int this_m = m_full - numrem;
+            if(this_m >= 3 && this_S > 2LL * MX) {
+                ans = max(ans, this_S);
             }
         }
-        for(auto x : a) {
-            if(freq[x] > 0) leftovers.push_back(x);
-        }
-
-        // take largest leftover as middle if exists
-        if(!leftovers.empty()) pairSides.push_back(leftovers[0]);
-
-        if(pairSides.size() < 3) {
-            cout << 0 << "\n";
-            continue;
-        }
-
-        sort(pairSides.begin(), pairSides.end());
-        long long sum = accumulate(pairSides.begin(), pairSides.end(),0LL);
-        long long longest = pairSides.back();
-
-        // check convexity, if fails remove smallest pair iteratively
-        int idx = 0;
-        while(longest >= sum - longest && pairSides.size()>=3){
-            if(pairSides.size()>=4){
-                sum -= pairSides[idx] + pairSides[idx+1];
-                idx += 2;
-            } else break;
-            longest = pairSides.back();
-        }
-
-        if(longest >= sum - longest) cout << 0 << "\n";
-        else cout << sum << "\n";
+        cout << ans << "\n";
     }
+    return 0;
 }
