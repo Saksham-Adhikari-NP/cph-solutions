a5956f1e6260a9116ac330a5aeab1a5aeb4b996d
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-10-10 20:44:29 +0545

    Auto-commit: Updated B_Bitwise_Reversion.cpp

--- B_Bitwise_Reversion.cpp
+++ B_Bitwise_Reversion.cpp
@@ -1,19 +1,29 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define int long long
-#define endl '\n'
+#include <iostream>
 
-int32_t main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    int t;
-    cin >> t;
-    while (t--) {
-        // solve here
-
-
+void solve() {
+    int x, y, z;
+    if (!(std::cin >> x >> y >> z)) {
+        return;
     }
-    
+
+    bool cond1 = ((x & y) | z) == z;
+    bool cond2 = ((y & z) | x) == x;
+    bool cond3 = ((x & z) | y) == y;
+
+    if (cond1 && cond2 && cond3) {
+        std::cout << "YES\n";
+    } else {
+        std::cout << "NO\n";
+    }
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    int t;
+    std::cin >> t;
+    while (t--) {
+        solve();
+    }
     return 0;
 }
