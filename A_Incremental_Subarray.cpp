1cf0b40240a9db1aa52e4b719c8d4908fb07c649
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-10-10 20:53:16 +0545

    Auto-commit: Updated A_Incremental_Subarray.cpp

--- A_Incremental_Subarray.cpp
+++ A_Incremental_Subarray.cpp
@@ -1,40 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
-#define endl '\n'
 
-void solve() {
-    int n;
-    cin >> n;
-
-    // We use a std::set to store only the unique beauty values.
-    // IMPORTANT: This set must be created fresh for each test case.
-    // By declaring it here, it's re-created every time solve() is called.
-    set<int> unique_beauties;
-
-    // This loop MUST run 'n' times to read all the apple beauties.
-    // This is the most likely place where an error might be.
-    for (int i = 0; i < n; ++i) {
-        int b;
-        cin >> b;
-        unique_beauties.insert(b);
-    }
-
-    // The size of the set is the number of distinct beauties, which is our answer.
-    cout << unique_beauties.size() << endl;
-}
-
-int32_t main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
+int main() {
     int t;
     cin >> t;
-    while (t--) {
-        // Putting the logic in a separate function like this is clean
-        // and helps avoid mistakes like declaring variables outside the loop.
-        solve();
+    for (int test = 0; test < t; ++test) {
+        int n;
+        cin >> n;
+        set<int> unique_beauties;
+        for (int i = 0; i < n; ++i) {
+            int b;
+            cin >> b;
+            unique_beauties.insert(b);
+        }
+        cout << unique_beauties.size() << endl;
     }
-    
     return 0;
 }
