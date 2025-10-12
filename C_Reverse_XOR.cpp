acdbccee52cec5531d8804a737642df24459ea59
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-10-12 21:09:21 +0545

    Auto-commit: Updated C_Reverse_XOR.cpp

--- C_Reverse_XOR.cpp
+++ C_Reverse_XOR.cpp
@@ -3,21 +3,32 @@ using namespace std;
 #define int long long
 #define endl '\n'
 
-// check if n is a power of two
-bool isPowerOfTwo(int n) {
-    return n && ((n & (n - 1)) == 0);
+// reverse binary without leading zeros
+int reverseBinary(int x) {
+    int y = 0;
+    while (x) {
+        y = (y << 1) | (x & 1);
+        x >>= 1;
+    }
+    return y;
 }
 
 int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     
+    // precompute all possible x ^ f(x) for x < 65536
+    unordered_set<int> possible;
+    for (int x = 1; x < (1 << 16); x++) {
+        possible.insert(x ^ reverseBinary(x));
+    }
+    
     int t;
     cin >> t;
     while (t--) {
         int n;
         cin >> n;
-        if (n == 0 || !isPowerOfTwo(n)) cout << "YES" << endl;
+        if (possible.count(n)) cout << "YES" << endl;
         else cout << "NO" << endl;
     }
     
