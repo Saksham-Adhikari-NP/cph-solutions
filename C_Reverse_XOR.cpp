4502321c14b7d5e91af01cd79bad56871c744e3b
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-10-12 21:07:00 +0545

    Auto-commit: Updated C_Reverse_XOR.cpp

--- C_Reverse_XOR.cpp
+++ C_Reverse_XOR.cpp
@@ -3,25 +3,23 @@ using namespace std;
 #define int long long
 #define endl '\n'
 
+// check if n is a power of two
+bool isPowerOfTwo(int n) {
+    return n && ((n & (n - 1)) == 0);
+}
+
 int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
+    
     int t;
     cin >> t;
     while (t--) {
         int n;
         cin >> n;
-        int count = 0;
-        while (n) {
-            n = n & (n - 1); // removes the lowest set bit
-            count ^= 1;       // toggle count parity
-        }
-        if (count == 0)
-            cout << "Even\n";
-        else
-            cout << "Odd\n";
+        if (n == 0 || !isPowerOfTwo(n)) cout << "YES" << endl;
+        else cout << "NO" << endl;
     }
-
+    
     return 0;
 }
