01330bdab29a3fa111f96ef271b679665eedea6d
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-10-12 21:05:59 +0545

    Auto-commit: Updated C_Reverse_XOR.cpp

--- C_Reverse_XOR.cpp
+++ C_Reverse_XOR.cpp
@@ -3,36 +3,25 @@ using namespace std;
 #define int long long
 #define endl '\n'
 
-// reverse binary without leading zeros
-int reverseBinary(int x) {
-    int y = 0;
-    while (x) {
-        y = (y << 1) | (x & 1);
-        x >>= 1;
-    }
-    return y;
-}
-
-// check if there exists x such that x ^ f(x) = n
-bool exists(int n) {
-    for (int x = 1; x <= (1 << 20); x++) { // 2^20 is safe for small n
-        if ((x ^ reverseBinary(x)) == n) return true;
-    }
-    return false;
-}
-
 int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
     int t;
     cin >> t;
     while (t--) {
         int n;
         cin >> n;
-        if (exists(n)) cout << "YES" << endl;
-        else cout << "NO" << endl;
+        int count = 0;
+        while (n) {
+            n = n & (n - 1); // removes the lowest set bit
+            count ^= 1;       // toggle count parity
+        }
+        if (count == 0)
+            cout << "Even\n";
+        else
+            cout << "Odd\n";
     }
-    
+
     return 0;
 }
