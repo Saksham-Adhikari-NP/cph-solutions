eeb257f3ce177d2b132138e363880a06af4b48f9
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-10-12 21:17:42 +0545

    Auto-commit: Updated A_MEX_Partition.cpp

--- A_MEX_Partition.cpp
+++ A_MEX_Partition.cpp
@@ -6,31 +6,39 @@ using namespace std;
 int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
     int t;
     cin >> t;
     while (t--) {
         int n;
         cin >> n;
-        vector<int> a(n), cnt(101, 0);
-        for (int i = 0; i < n; i++) {
-            cin >> a[i];
-            cnt[a[i]]++;
+        vector<int> freq(102,0);
+        for(int i=0;i<n;i++){
+            int x;
+            cin >> x;
+            freq[x]++;
         }
 
-        int ans = 0;
-        for (int i = 0; i <= 100; i++) {
-            if (cnt[i] == 0) {
-                ans = i;
+        int first_missing = 0, second_missing = 0;
+
+        // first pass: count mex considering 0 or 1 occurrence
+        for(int i=0;i<=101;i++){
+            if(freq[i]==0){
+                first_missing = i;
                 break;
+            } else {
+                freq[i]--; // take one occurrence for first multiset
             }
-            if (cnt[i] == 1) {
-                ans = i + 1;
+        }
+
+        for(int i=0;i<=101;i++){
+            if(freq[i]==0){
+                second_missing = i;
                 break;
             }
         }
 
-        cout << ans << endl;
+        cout << first_missing + second_missing << endl;
     }
     return 0;
 }
