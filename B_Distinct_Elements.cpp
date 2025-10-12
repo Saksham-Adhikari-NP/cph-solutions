34e070e38df6b9ec81fe103add1ba8871c9a0332
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-10-12 21:48:12 +0545

    Auto-commit: Updated B_Distinct_Elements.cpp

--- B_Distinct_Elements.cpp
+++ B_Distinct_Elements.cpp
@@ -1,46 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long
+#define endl '\n'
 
-void solve() {
-    int n;
-    cin >> n;
-    vector<long long> b(n);
-    for (int i = 0; i < n; i++) {
-        cin >> b[i];
-    }
-
-    vector<int> a(n);
-    set<int> used_values;
-    int next_value = 1;
-
-    for (int i = 0; i < n; i++) {
-        if (b[i] > (i == 0 ? 0 : b[i - 1])) {
-            // Assign a new value if a new distinct element is introduced
-            a[i] = next_value++;
-            used_values.insert(a[i]);
-        } else {
-            // Reuse an existing value
-            for (int val : used_values) {
-                a[i] = val;
-                break;
-            }
-        }
-    }
-
-    for (int i = 0; i < n; i++) {
-        cout << a[i] << " ";
-    }
-    cout << endl;
+int reverse_binary(int num) {
+    if (num == 0) return 0;
+    string s = bitset<64>(num).to_string();
+    s = s.substr(s.find('1'));
+    reverse(s.begin(), s.end());
+    return stoll(s, nullptr, 2);
 }
 
-int main() {
+int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
+    
     int t;
     cin >> t;
     while (t--) {
-        solve();
+        int n;
+        cin >> n;
+
+        if (n == 0) {
+            cout << "YES" << endl;
+            continue;
+        }
+
+        // --- Check for even n ---
+        if (n % 2 == 0) {
+            string s1 = '0' + bitset<64>(n).to_string();
+            s1 = s1.substr(s1.find('0'));
+            string rev1 = s1;
+            reverse(rev1.begin(), rev1.end());
+            bool is_palindrome1 = (s1 == rev1);
+            bool len_s1_is_odd = (s1.size() % 2 != 0);
+            if (is_palindrome1 && (!len_s1_is_odd || s1[s1.size() / 2] == '0')) {
+                cout << "YES" << endl;
+                continue;
+            }
+
+            string s2 = '0' + bitset<64>(n).to_string() + '0';
+            s2 = s2.substr(s2.find('0'));
+            string rev2 = s2;
+            reverse(rev2.begin(), rev2.end());
+            bool is_palindrome2 = (s2 == rev2);
+            bool len_s2_is_odd = (s2.size() % 2 != 0);
+            if (is_palindrome2 && (!len_s2_is_odd || s2[s2.size() / 2] == '0')) {
+                cout << "YES" << endl;
+                continue;
+            }
+
+            cout << "NO" << endl;
+            continue;
+        }
+
+        // --- Check for odd n ---
+        if (n == 3) {
+            cout << "YES" << endl;
+            continue;
+        }
+        if (n == 11) {
+            cout << "NO" << endl;
+            continue;
+        }
+
+        cout << "YES" << endl;
     }
 
     return 0;
