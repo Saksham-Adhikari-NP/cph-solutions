751770909a7a5444120015215c9c0f875d36c62d
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-10-12 21:33:44 +0545

    Auto-commit: Updated B_Distinct_Elements.cpp

--- B_Distinct_Elements.cpp
+++ B_Distinct_Elements.cpp
@@ -9,33 +9,33 @@ int32_t main() {
 
     int t;
     cin >> t;
-    while (t--) {
+    while(t--) {
         int n;
         cin >> n;
         vector<long long> b(n);
-        for (int i = 0; i < n; i++) cin >> b[i];
+        for(int i=0;i<n;i++) cin >> b[i];
 
         vector<int> a(n);
-        deque<int> reused;          // numbers already assigned
-        int next_new = 1;           // next new number to assign
+        deque<int> pool;  // pool of available numbers to repeat
+        int next_new = 1; // next new number to assign
 
         long long last = 0;
-        for (int i = 0; i < n; i++) {
+        for(int i=0;i<n;i++){
             long long delta = b[i] - last;
             last = b[i];
 
-            if (delta > 0) {
-                // Assign a new number
+            if(delta > 0){
+                // Assign a new number from the pool of unused numbers
                 a[i] = next_new;
-                reused.push_back(next_new); // add to reused pool
+                pool.push_back(next_new);
                 next_new++;
             } else {
-                // Assign any number from reused pool
-                a[i] = reused.back(); // pick most recently assigned number
+                // Assign the most recently assigned number (from pool)
+                a[i] = pool.back();
             }
         }
 
-        for (int i = 0; i < n; i++) cout << a[i] << " ";
+        for(int i=0;i<n;i++) cout << a[i] << " ";
         cout << endl;
     }
 
