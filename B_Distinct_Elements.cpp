9620e7e8c8808d27c78248bf7bec97677229daef
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-10-14 00:03:22 +0545

    Auto-commit: Updated B_Distinct_Elements.cpp

--- B_Distinct_Elements.cpp
+++ B_Distinct_Elements.cpp
@@ -3,68 +3,38 @@ using namespace std;
 #define int long long
 #define endl '\n'
 
-int reverse_binary(int num) {
-    if (num == 0) return 0;
-    string s = bitset<64>(num).to_string();
-    s = s.substr(s.find('1'));
-    reverse(s.begin(), s.end());
-    return stoll(s, nullptr, 2);
-}
-
 int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
     int t;
     cin >> t;
     while (t--) {
         int n;
         cin >> n;
+        vector<long long> b(n + 1);
+        for (int i = 1; i <= n; ++i) cin >> b[i];
 
-        if (n == 0) {
-            cout << "YES" << endl;
-            continue;
-        }
+        vector<int> a(n + 1);
+        int next_new = 1; 
 
-        // --- Check for even n ---
-        if (n % 2 == 0) {
-            string s1 = '0' + bitset<64>(n).to_string();
-            s1 = s1.substr(s1.find('0'));
-            string rev1 = s1;
-            reverse(rev1.begin(), rev1.end());
-            bool is_palindrome1 = (s1 == rev1);
-            bool len_s1_is_odd = (s1.size() % 2 != 0);
-            if (is_palindrome1 && (!len_s1_is_odd || s1[s1.size() / 2] == '0')) {
-                cout << "YES" << endl;
-                continue;
+        a[1] = 1; 
+        for (int i = 2; i <= n; ++i) {
+            long long diff = b[i] - b[i - 1];
+
+            if (diff == i) {
+                
+                ++next_new;
+                a[i] = next_new;
+            } else {
+               
+                a[i] = a[i - diff];
             }
-
-            string s2 = '0' + bitset<64>(n).to_string() + '0';
-            s2 = s2.substr(s2.find('0'));
-            string rev2 = s2;
-            reverse(rev2.begin(), rev2.end());
-            bool is_palindrome2 = (s2 == rev2);
-            bool len_s2_is_odd = (s2.size() % 2 != 0);
-            if (is_palindrome2 && (!len_s2_is_odd || s2[s2.size() / 2] == '0')) {
-                cout << "YES" << endl;
-                continue;
-            }
-
-            cout << "NO" << endl;
-            continue;
         }
 
-        // --- Check for odd n ---
-        if (n == 3) {
-            cout << "YES" << endl;
-            continue;
-        }
-        if (n == 11) {
-            cout << "NO" << endl;
-            continue;
-        }
-
-        cout << "YES" << endl;
+        for (int i = 1; i <= n; ++i)
+            cout << a[i] << " ";
+        cout << endl;
     }
 
     return 0;
