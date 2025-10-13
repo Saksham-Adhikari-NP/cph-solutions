ba8703474e356f353a050fae3fd8e4d7b99832e7
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-10-14 00:05:04 +0545

    Auto-commit: Updated B_Distinct_Elements.cpp

--- B_Distinct_Elements.cpp
+++ B_Distinct_Elements.cpp
@@ -13,27 +13,21 @@ int32_t main() {
         int n;
         cin >> n;
         vector<long long> b(n + 1);
+        b[0] = 0;
         for (int i = 1; i <= n; ++i) cin >> b[i];
 
-        vector<int> a(n + 1);
-        int next_new = 1; 
+        vector<int> res;
+        res.reserve(n);
 
-        a[1] = 1; 
-        for (int i = 2; i <= n; ++i) {
-            long long diff = b[i] - b[i - 1];
-
-            if (diff == i) {
-                
-                ++next_new;
-                a[i] = next_new;
-            } else {
-               
-                a[i] = a[i - diff];
-            }
+        for (int i = 0; i < n; ++i) {
+            long long diff = b[i + 1] - b[i];
+            if (i - diff >= 0)
+                res.push_back(res[i - diff]);
+            else
+                res.push_back(i + 1);
         }
 
-        for (int i = 1; i <= n; ++i)
-            cout << a[i] << " ";
+        for (int x : res) cout << x << " ";
         cout << endl;
     }
 
