509b264a44f53b6b131b816c520a4a7059634053
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-10-14 18:53:40 +0545

    Auto-commit: Updated B_Distinct_Elements.cpp

--- B_Distinct_Elements.cpp
+++ B_Distinct_Elements.cpp
@@ -1,35 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
-#define endl '\n'
-
-int32_t main() {
+typedef long long ll;
+ 
+void solve() {
+    int t;cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        vector<ll> b(n + 1, 0);
+        for (int i = 1; i <= n; i++) cin >> b[i];
+        vector<int> a(n + 1, 0);
+        int cur = 1;
+        for (int i = 1; i <= n; i++) {
+            ll d = b[i] - b[i - 1];
+            ll p = i - d;
+            if (p == 0) a[i] = cur++;
+            else a[i] = a[(int)p];}
+        for (int i = 1; i <= n; i++) {
+            if (i > 1) cout << ' ';
+            cout << a[i];}
+        cout << '\n';}}
+ 
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<long long> b(n + 1);
-        b[0] = 0;
-        for (int i = 1; i <= n; ++i) cin >> b[i];
-
-        vector<int> res;
-        res.reserve(n);
-
-        for (int i = 0; i < n; ++i) {
-            long long diff = b[i + 1] - b[i];
-            if (i - diff >= 0)
-                res.push_back(res[i - diff]);
-            else
-                res.push_back(i + 1);
-        }
-
-        for (int x : res) cout << x << " ";
-        cout << endl;
-    }
-
-    return 0;
-}
+    solve();}
