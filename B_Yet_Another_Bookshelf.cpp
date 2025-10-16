36d3e782c4e18c607498c54dc29d2bb73fee8806
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-10-16 23:51:41 +0545

    Auto-commit: Updated B_Yet_Another_Bookshelf.cpp

--- B_Yet_Another_Bookshelf.cpp
+++ B_Yet_Another_Bookshelf.cpp
@@ -11,6 +11,23 @@ int32_t main() {
     cin >> t;
     while (t--) {
         // solve here
+        int n; cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        int first = -1, last = -1;
+        for (int i = 0; i < n; i++) {
+            if (a[i] == 1) {
+                if (first == -1) first = i;
+                last = i;
+            }
+        }
+
+        int moves = 0;
+        for (int i = first; i <= last; i++)
+            if (a[i] == 0) moves++;
+
+        cout << moves << "\n";
 
 
     }
