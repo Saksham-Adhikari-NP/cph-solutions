7fec7bac79f4f9c5dbcba203e33a32fc5a1ab3df
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-10-16 20:20:04 +0545

    Auto-commit: Updated C_Maximum_Median.cpp

--- C_Maximum_Median.cpp
+++ C_Maximum_Median.cpp
@@ -6,39 +6,28 @@ using namespace std;
 int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, k;
-        cin >> n >> k;
-        vector<int> a(n);
-        for (int i = 0; i < n; i++) cin >> a[i];
-        
-        sort(a.begin(), a.end());
-        int m = n / 2; // median index
 
-        // Binary search on the maximum possible median
-        int low = a[m], high = a[m] + k + 1;
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (auto &x : a) cin >> x;
 
-        while (low < high) {
-            int mid = (low + high) / 2;
+    sort(a.begin(), a.end());
+    int m = n / 2; // median index
+    int low = a[m], high = a[m] + k + 1; // +1 for binary search boundary
 
-            // Check if we can make median >= mid
-            int needed = 0;
-            for (int i = m; i < n; i++) {
-                if (a[i] < mid)
-                    needed += (mid - a[i]);
-            }
-
-            if (needed <= k)
-                low = mid + 1; // possible, try higher
-            else
-                high = mid;    // impossible, try lower
+    while (low < high) {
+        int mid = (low + high) / 2;
+        long long need = 0;
+        for (int i = m; i < n; i++) {
+            if (a[i] < mid) need += mid - a[i];
+            if (need > k) break; // early stop for efficiency
         }
 
-        cout << (low - 1) << endl;
+        if (need <= k) low = mid + 1;
+        else high = mid;
     }
-    
+
+    cout << low - 1 << endl;
     return 0;
 }
