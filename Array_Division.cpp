670ed83cbbed9be6d79bb1bafe1b94967a068a79
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-12-03 21:09:07 +0545

    Auto-commit: Array_Division.cpp

---
+++ Array_Division.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+
+template<typename T>
+istream& operator >> (istream& s, vector<T> & v){
+  for(auto &x: v)
+    s >> x;
+  return s;
+}
+template<typename T>
+ostream& operator << (ostream& s, vector<T> & v){
+  for(auto &x: v){
+    s << x << " ";
+  }
+  s << endl;
+  return s;
+}
+
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        // solve here
+
+
+    }
+    
+    return 0;
+}
