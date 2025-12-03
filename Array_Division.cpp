a485809d1db0371d32b80860c805f268c476d69b
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-12-03 23:25:38 +0545

    Auto-commit: Array_Division.cpp

--- Array_Division.cpp
+++ Array_Division.cpp
@@ -1,35 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
-#define endl '\n'
 
-template<typename T>
-istream& operator >> (istream& s, vector<T> & v){
-  for(auto &x: v)
-    s >> x;
-  return s;
-}
-template<typename T>
-ostream& operator << (ostream& s, vector<T> & v){
-  for(auto &x: v){
-    s << x << " ";
-  }
-  s << endl;
-  return s;
+bool canDivide(const vector<int>& a, int n, int k, long long limit) {
+    int parts = 1;
+    long long sum = 0;
+    for (int i = 0; i < n; i++) {
+        if (sum + a[i] <= limit) {
+            sum += a[i];
+        } else {
+            parts++;
+            sum = a[i];
+        }
+    }
+    return parts <= k;
 }
 
-
-int32_t main() {
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    int t;
-    cin >> t;
-    while (t--) {
-        // solve here
 
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
 
+    long long low = *max_element(a.begin(), a.end());
+    long long high = accumulate(a.begin(), a.end(), 0LL);
+
+    while (low < high) {
+        long long mid = low + (high - low) / 2;
+        if (canDivide(a, n, k, mid)) high = mid;
+        else low = mid + 1;
     }
-    
-    return 0;
+
+    cout << low << "\n";
 }
