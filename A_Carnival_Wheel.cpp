d6ad84a69a68cfefda7e0e53adebc831937b28a6
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-12-19 22:28:25 +0545

    Auto-commit: A_Carnival_Wheel.cpp

---
+++ A_Carnival_Wheel.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+const int INF = 1e9;
+const int MOD = 1e9+7; 
+
+template<typename T>
+istream& operator >> (istream& s, vector<T> & v){
+  for(auto &x: v)
+    s >> x;
+  return s;
+}
+template<typename T>
+ostream& operator << (ostream& s, vector<T> & v){
+  for(auto &x: v){
+    s << x << " ";
+  }
+  s << endl;
+  return s;
+}
+
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        // solve here
+        int l, a, b;
+        cin >> l >> a >> b;
+
+        int cur = a;
+        int ans = a;
+        for (int i = 0; i < l; i++) {
+            ans = max(ans, cur);
+            cur = (cur + b) % l;
+        }
+
+        cout << ans <<endl;
+    }
+    return 0;
+  }
