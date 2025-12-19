c43286e2b4da07c81cfe4c1d7b3433eec66b9e17
Saksham-Adhikari-NP <saksham71032@gmail.com>
2025-12-19 22:39:36 +0545

    Auto-commit: B_Ashmal.cpp

---
+++ B_Ashmal.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+const int INF = 1e9;
+const int MOD = 1e9+7; 
+
+template<typename T>
+istream& operator >> (istream& s, vector<T> & v){
+  for(auto &x: v)
+    s >> x;
+  return s;
+}
+template<typename T>
+ostream& operator << (ostream& s, vector<T> & v){
+  for(auto &x: v){
+    s << x << " ";
+  }
+  s << endl;
+  return s;
+}
+
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        // solve here
+        int n ;
+        cin >> n ;
+        vector<string> arr(n);
+        cin >> arr ;
+        string left , right , ans ; 
+        for(auto x: arr) 
+        {
+          left = x + ans ;
+          right = ans + x ;
+          if( left < right) ans = left ;
+          else ans = right ;  
+        }
+      cout << ans << endl ;
+
+
+    }
+    
+    return 0;
+}
