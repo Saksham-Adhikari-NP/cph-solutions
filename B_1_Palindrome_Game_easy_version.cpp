86997f144c9df6a072d4b70ae89dc8b31181c9fe
Saksham-Adhikari-NP <saksham71032@gmail.com>
2026-01-02 16:26:55 +0545

    Auto-commit: B_1_Palindrome_Game_easy_version.cpp

---
+++ B_1_Palindrome_Game_easy_version.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define int long long
+#define endl '\n'
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define all(a)               (a).begin(), (a).end()
+#define rall(a)              (a).rbegin(), (a).rend()
+#define foi(n) for(ll i=0;i<n;i++)
+#define foj(n) for(ll j=0;j<n;j++)
+#define fok(n) for(ll k=0;k<n;k++)
+#define sz(v) v.size()
+#define forr(i,a,b) for(ll i=a;i<b;i++)
+#define forrr(i,b,a) for(ll i=b;i>=a;i--)
+#define forrrr(i,a,b,k) for(ll i=a;i<b;i=i+k)
+
+
+
+const int INF = 1e9;
+const int MOD = 1e9+7; 
+
+template<typename T>
+istream& operator >> (istream& s, vector<T> & v){
+  for(auto &x: v)
+    s >> x;
+  return s;
+}
+template<typename T>
+ostream& operator << (ostream& s, vector<T> & v){
+  for(auto &x: v){
+    s << x << " ";
+  }
+  s << endl;
+  return s;
+}
+
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        // solve here
+      int n ;
+      cin >> n ;
+      string s ;
+      cin >> s ;
+      int zeroes = count(all(s) , '0') ;
+      if ( !(zeroes % 2) || zeroes == 1ll ) cout << "BOB" << endl;
+      else cout << "ALICE" << endl ;
+    }
+    
+    return 0;
+}
