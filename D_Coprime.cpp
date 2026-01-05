010aeaa26cd2f09bbcc0170ce8e317b4cf6ba1f0
Saksham-Adhikari-NP <saksham71032@gmail.com>
2026-01-05 23:43:26 +0545

    Auto-commit: D_Coprime.cpp

---
+++ D_Coprime.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define int long long
+#define endl '\n'
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define foi(n) for(ll i=0;i<n;i++)
+#define foj(n) for(ll j=0;j<n;j++)
+#define fok(n) for(ll k=0;k<n;k++)
+#define sz(v) v.size()
+#define forr(i,a,b) for(ll i=a;i<b;i++)
+#define forrr(i,b,a) for(ll i=b;i>=a;i--)
+#define forrrr(i,a,b,k) for(ll i=a;i<b;i=i+k)
+
+const int INF = 1e18;
+const int MOD = 1e9+7; 
+
+// Modular operations
+int modpow(int x, int n, int m = MOD) {
+    if (n == 0) return 1%m;
+    int u = modpow(x, n/2, m);
+    u = (u * 1LL * u) % m;
+    if (n%2 == 1) u = (u * 1LL * x) % m;
+    return u;
+}
+int add(int a, int b, int m = MOD) { return (a + b) % m; }
+int sub(int a, int b, int m = MOD) { return (a - b + m) % m; }
+int mul(int a, int b, int m = MOD) { return (a * 1LL * b) % m; }
+int modinv(int a, int m = MOD) { return modpow(a, m-2, m); }
+int moddiv(int a, int b, int m = MOD) { return mul(a, modinv(b,m), m); }
+int norm(int x, int m = MOD) { x %= m; if(x < 0) x += m; return x; }
+
+// nCr
+const int MAXN = 1e6+5;
+int fact[MAXN], invfact[MAXN];
+void init_fact() {
+    fact[0] = 1;
+    forr(i,1,MAXN) fact[i] = mul(fact[i-1], i);
+    invfact[MAXN-1] = modinv(fact[MAXN-1]);
+    forrrr(i,MAXN-2,0,1) invfact[i] = mul(invfact[i+1], i+1);
+}
+int nCr(int n, int r) {
+    if(r<0 || r>n) return 0;
+    return mul(fact[n], mul(invfact[r], invfact[n-r]));
+}
+
+// vector io
+template<typename T>
+istream& operator >> (istream& s, vector<T> & v){
+    for(auto &x: v) s >> x;
+    return s;
+}
+template<typename T>
+ostream& operator << (ostream& s, vector<T> & v){
+    for(auto &x: v) s << x << " ";
+    s << endl;
+    return s;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // init_fact(); // Uncomment if nCr needed
+
+    vector<int> pairs[1001];
+
+
+    for(int i = 1; i <= 1000; ++i) {
+        for(int j = 1; j <= 1000; ++j) {
+            if(__gcd(i, j) == 1) {
+                    pairs[i].push_back(j);
+                }
+            }
+        }
+
+
+    int t ; cin >>  t ;
+    while (t--) {
+        // solve here
+
+    int n; cin >> n;
+    vector<int> id[1001];
+    for(int i = 1; i <= n; ++i) {
+        int x; cin >> x;
+        id[x].push_back(i);
+    }
+    int ans = -1;
+    for(int i = 1; i <= 1000; ++i) {
+        for(int j: pairs[i]) {
+            if(!id[i].empty() && !id[j].empty()) {
+                ans = max(ans, id[i].back() + id[j].back());
+            }
+        }
+    }
+    cout << ans << "\n";
+
+
+    }
+
+    return 0;
+}
+
