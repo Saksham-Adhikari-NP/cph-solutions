7c08282785bddbff9572c8993fe47792bcdb86ed
Saksham-Adhikari-NP <saksham71032@gmail.com>
2026-01-14 23:57:51 +0545

    Auto-commit: C_Chat_Ban.cpp

---
+++ C_Chat_Ban.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define int long long
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define sz(v) v.size()
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a,x) for (auto& a : x)
+#define formul(i,a,b,k) for(ll i=a;i<b;i=i+k)
+
+const int INF = 1e18;
+const int MOD = LLONG_MAX; 
+
+// Modular operations
+int modpow(int x, int n, int m = MOD) {
+    if (n == 0) return 1%m;
+    int u = modpow(x, n/2, m);
+    u = (u * 1LL * u) % m;
+    if (n%2 == 1) u = (u * 1LL * x) % m;
+    return u;
+}
+int add(int a, int b, int m = MOD) { return (a + b) % m; }
+int sub(int a, int b, int m = MOD) { return (a - b + m) % m; }
+int mul(int a, int b, int m = MOD) { return (a * 1LL * b) % m; }
+int modinv(int a, int m = MOD) { return modpow(a, m-2, m); }
+int moddiv(int a, int b, int m = MOD) { return mul(a, modinv(b,m), m); }
+int norm(int x, int m = MOD) { x %= m; if(x < 0) x += m; return x; }
+
+// nCr
+const int MAXN = 1e6+5;
+int fact[MAXN], invfact[MAXN];
+void init_fact() {
+    fact[0] = 1;
+    for(int i = 1; i < MAXN; i++) fact[i] = mul(fact[i-1], i);
+    invfact[MAXN-1] = modinv(fact[MAXN-1]);
+    for(int i = MAXN-2; i >= 0; i--) invfact[i] = mul(invfact[i+1], i+1);
+}
+int nCr(int n, int r) {
+    if(r<0 || r>n) return 0;
+    return mul(fact[n], mul(invfact[r], invfact[n-r]));
+}
+
+// vector io
+template<typename T>
+istream& operator >> (istream& s, vector<T> & v){
+    for(auto &x: v) s >> x;
+    return s;
+}
+template<typename T>
+ostream& operator << (ostream& s, vector<T> & v){
+    for(auto &x: v) s << x << " ";
+    s << endl;
+    return s;
+}
+long long get(int x) {
+    return x * 1ll * (x + 1) / 2;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // init_fact(); // Uncomment if nCr needed
+
+    int t;
+    cin >> t;
+    while (t--) {
+        // solve here
+        int k;
+        long long x;
+        cin >> k >> x;
+        long long l = 1, r = 2 * k - 1;
+        long long res = 2 * k - 1;
+        bool over = false;
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            if (mid >= k) {
+                over = (get(k) + get(k - 1) - get(2 * k - 1 - mid) >= x);
+            } else {
+                over = (get(mid) >= x);
+            }
+            if (over) {
+                res = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        cout << res << endl;
+
+    }
+
+        
+
+    return 0;
+}
+
