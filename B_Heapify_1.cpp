c594c776d7409318f22c3270f145794e26dced02
Saksham-Adhikari-NP <saksham71032@gmail.com>
2026-02-18 20:54:15 +0545

    Auto-commit: B_Heapify_1.cpp

---
+++ B_Heapify_1.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define int long long
+#define endl '\n'
+#define vi vector<int>  
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define Max(x) (*max_element(all(x)))
+#define Min(x) (*min_element(all(x)))
+#define sz(x) ((int)x.size())
+#define Unique(x) sort(all(x)); (x).erase(unique(all(x)),x.end())
+
+// bitmanip shortcuts 
+int hsetbit (int a ) { return (63 - __builtin_clzll(a)) ; } 
+int lsetbit (int n ) { return (n & -n) ; }
+int setbit (int n) { return __builtin_popcountll(n); } 
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a,x) for (auto& a : x)
+#define formul(i,a,b,k) for(ll i=a;i<b;i=i+k)
+
+const int INF = 1e18;
+const int MOD = 1e9+7; 
+
+// Modular operations
+int modpow(int x, int n, int m = MOD) {
+    if (n == 0) return 1%m;
+    int u = modpow(x, n/2, m);
+    u = (u * 1LL * u) % m;
+    if (n%2 == 1) u = (u * 1LL * x) % m;
+    return u;
+}
+int add(int a, int b, int m = MOD) { return (a + b) % m; }
+int sub(int a, int b, int m = MOD) { return (a - b + m) % m; }
+int mul(int a, int b, int m = MOD) { return (a * 1LL * b) % m; }
+int modinv(int a, int m = MOD) { return modpow(a, m-2, m); }
+int moddiv(int a, int b, int m = MOD) { return mul(a, modinv(b,m), m); }
+int norm(int x, int m = MOD) { x %= m; if(x < 0) x += m; return x; }
+
+// nCr
+const int MAXN = 1e6+5;
+int fact[MAXN], invfact[MAXN];
+void init_fact() {
+    fact[0] = 1;
+    for(int i = 1; i < MAXN; i++) fact[i] = mul(fact[i-1], i);
+    invfact[MAXN-1] = modinv(fact[MAXN-1]);
+    for(int i = MAXN-2; i >= 0; i--) invfact[i] = mul(invfact[i+1], i+1);
+}
+int nCr(int n, int r) {
+    if(r<0 || r>n) return 0;
+    return mul(fact[n], mul(invfact[r], invfact[n-r]));
+}
+
+// vector io
+template<typename T>
+istream& operator >> (istream& s, vector<T> & v){
+    for(auto &x: v) s >> x;
+    return s;
+}
+template<typename T>
+ostream& operator << (ostream& s, vector<T> & v){
+    for(auto &x: v) s << x << " ";
+    s << endl;
+    return s;
+}
+int odd(int x) {
+    while (x % 2 == 0) x /= 2;
+    return x;
+}
+
+void solve () 
+{
+    // solve here
+    // ai ,a2i can swap ; if i != a[i]; we can check if i can be alloted to its corerct position || if a[i] can go to the aith idx ; orrr ;;; 
+    // i *2 pow smth can be a[i] // if they are part of same chain like , x *2pow 4 ; x*2pow 3 ;; remove highest set bit and check if they are equal  
+    int n ; cin >> n ;
+    vi arr(n+1)  ; FOR(i,1,n+1) cin >> arr[i] ; bool ans = true ; 
+    FOR(i,1,n+1) if (odd(i) != odd(arr[i]) )
+    {
+        ans = false ; 
+        break ; 
+    }
+cout <<(ans? "YES" : "NO" ) << endl ; 
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+   // freopen("input.txt", "r", stdin);   // debug
+
+    // init_fact(); // Uncomment if nCr needed
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve() ; 
+    }
+
+    return 0;
+}
+
