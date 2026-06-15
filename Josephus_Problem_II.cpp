aaac34f64945f226203eb8c7973d0391d2c14570
Saksham-Adhikari-NP <saksham71032@gmail.com>
2026-06-15 23:28:20 +0545

    Auto-commit: Josephus_Problem_II.cpp

--- Josephus_Problem_II.cpp
+++ Josephus_Problem_II.cpp
@@ -1,82 +1,141 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define mod 1000000007
 
-// Function to print the order in which children are removed
-void solve(int N, int K)
-{
-    // 2D array to store ranges of size sqrt(N)
-    vector<vector<int> > arr;
 
-    int root = sqrt(N);
-    int row = 0, col = 0, count = 0;
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
 
-    // Construct the 2D vector arr
-    vector<int> vec;
-    for (int i = 1; i <= N; i++) {
-        if (count > root) {
-            count = 0;
-            arr.push_back(vec);
-            vec.clear();
-        }
-        vec.push_back(i);
-        count++;
-    }
-    if (!vec.empty())
-        arr.push_back(vec);
 
-    // Iterate till we have removed all the children
-    for (int i = 0; i < N; i++) {
-        // Fnd the position of the element to be removed
-        int j = K % (N - i);
+#define int long long
+#define endl '\n'
+#define vi vector<int>  
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define Max(x) (*max_element(all(x)))
+#define Min(x) (*min_element(all(x)))
+#define Sum(x) (accumulate(all(x), 0LL))
+#define sz(x) ((int)x.size())
+#define Unique(x) sort(all(x)); (x).erase(unique(all(x)),x.end())
 
-        // Make jumps till we reach the position of the
-        // element to be removed
-        while (j) {
-            // If we can jump j elements in the current row,
-            // we jump to that column
-            if (col + j < arr[row].size()) {
-                col += j;
-                j = 0;
-            }
-            // If we cannot jump j elements, we jump over
-            // all the elements in the current row and move
-            // to the next row
-            else {
-                j -= arr[row].size() - col;
-                col = 0;
-                row++;
-            }
-            // If all the elements are traversed, we start
-            // from the first row again
-            if (row >= arr.size())
-                row = 0;
-        }
+// bitmanip shortcuts 
+int hsetbit (int a ) { return (63 - __builtin_clzll(a)) ; } 
+int lsetbit (int n ) { return (n & -n) ; }
+int setbit (int n) { return __builtin_popcountll(n); } 
 
-        // While the current row has lesser columns, move to
-        // the next row
-        cout << arr[row][col] << " ";
-        if (i != N - 1) {
-            // Remove the student from the current row
-            arr[row].erase(arr[row].begin() + col);
-            while (arr[row].size() <= col) {
-                col = 0;
-                row++;
-                if (row >= arr.size())
-                    row = 0;
-            }
-        }
-    }
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define trav(a,x) for (auto& a : x)
+
+const int INF = 1e18;
+const int MOD = 1e9+7; 
+
+// Modular operations
+int modpow(int x, int n, int m = MOD) {
+    if (n == 0) return 1%m;
+    int u = modpow(x, n/2, m);
+    u = (u * 1LL * u) % m;
+    if (n%2 == 1) u = (u * 1LL * x) % m;
+    return u;
+}
+int add(int a, int b, int m = MOD) { return (a + b) % m; }
+int sub(int a, int b, int m = MOD) { return (a - b + m) % m; }
+int mul(int a, int b, int m = MOD) { return (a * 1LL * b) % m; }
+int modinv(int a, int m = MOD) { return modpow(a, m-2, m); }
+int moddiv(int a, int b, int m = MOD) { return mul(a, modinv(b,m), m); }
+int norm(int x, int m = MOD) { x %= m; if(x < 0) x += m; return x; }
+
+// nCr
+const int MAXN = 1e6+5;
+int fact[MAXN], invfact[MAXN];
+void init_fact() {
+    fact[0] = 1;
+    for(int i = 1; i < MAXN; i++) fact[i] = mul(fact[i-1], i);
+    invfact[MAXN-1] = modinv(fact[MAXN-1]);
+    for(int i = MAXN-2; i >= 0; i--) invfact[i] = mul(invfact[i+1], i+1);
+}
+int nCr(int n, int r) {
+    if(r<0 || r>n) return 0;
+    return mul(fact[n], mul(invfact[r], invfact[n-r]));
 }
 
-int main()
+// vector io
+template<typename T>
+istream& operator >> (istream& s, vector<T> & v){
+    for(auto &x: v) s >> x;
+    return s;
+}
+template<typename T>
+ostream& operator << (ostream& s, vector<T> & v){
+    for(auto &x: v) s << x << " ";
+    s << endl;
+    return s;
+}
+inline void printYN(bool t) { cout << (t ? "YES" : "NO" ) << endl; }
+
+struct Edge {
+    int to;
+    long long weight;
+};
+
+using Graph = std::vector<std::vector<int>>;
+using WeightedGraph = std::vector<std::vector<Edge>>;
+
+ int dx[]={0,0,1,-1};
+ int dy[]={1,-1,0,0};
+ string ds="RLDU";
+int get_dir_idx(char c) {
+    if (c == 'R') return 0;
+    if (c == 'L') return 1;
+    if (c == 'D') return 2;
+    if (c == 'U') return 3;
+    return -1;
+}
+
+using pii = pair<int, int>;
+
+
+void solve () 
 {
-    // Sample Input
+    // solve here
+    
+        int n, k;
+    cin >> n >> k;
 
-    int N , K;
-    cin >>N>>K;
-    solve(N, K);
+    indexed_set st;
+    for (int i = 1; i <= n; i++) st.insert(i);
 
+    int pos = 0;
+
+    while (!st.empty())
+    {
+        pos = (pos + k) % sz(st);
+
+        auto it = st.find_by_order(pos);
+        cout << *it << ' ';
+
+        st.erase(it);
+    }
+
+    cout << endl;
+
+
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+   // freopen("input.txt", "r", stdin);   // debug
+
+    // init_fact(); // Uncomment if nCr needed
+
+solve() ; 
     return 0;
 }
