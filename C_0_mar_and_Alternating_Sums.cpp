2a0c35273a060acfbb5a222b9f16e573b31570ad
Saksham-Adhikari-NP <saksham71032@gmail.com>
2026-07-13 00:33:09 +0545

    Auto-commit: C_0_mar_and_Alternating_Sums.cpp

---
+++ C_0_mar_and_Alternating_Sums.cpp
@@ -0,0 +1,134 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+//ordered set; 
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
+
+#define int long long
+#define vi vector<int>  
+#define all(a) (a).begin(), (a).end() 
+#define rall(a) (a).rbegin(), (a).rend()
+#define Max(x) (*max_element(all(x)))
+#define Min(x) (*min_element(all(x)))
+#define sz(x) ((int)x.size())
+#define Unique(x) sort(all(x)); (x).erase(unique(all(x)),x.end())
+
+// bitmanip shortcuts 
+int hsetbit (int a ) { return (63 - __builtin_clzll(a)) ; } 
+int lsetbit (int n ) { return (n & -n) ; }
+int setbit (int n) { return __builtin_popcountll(n); } 
+
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+
+const int MAXN = 2e5+5;
+const int INF = 1e18;
+const int MOD = 1e9+7; 
+
+
+// vector io
+template<typename T>
+istream& operator >> (istream& s, vector<T>& v){ for(auto &x: v) s >> x; return s; }
+template<typename T>
+ostream& operator << (ostream& s, const vector<T>& v){ for(auto &x: v) s << x << ' '; return s; }
+
+inline void printYN(bool t) { cout << (t ? "YES" : "NO" ) << endl; }
+
+struct Edge {
+    int to;
+    long long weight;
+};
+
+using Graph = std::vector<std::vector<int>>;
+using WeightedGraph = std::vector<std::vector<Edge>>;
+
+ int dx[]={0,0,1,-1};
+ int dy[]={1,-1,0,0};
+ string ds="RLDU";
+int get_dir_idx(char c) {
+    if (c == 'R') return 0;
+    if (c == 'L') return 1;
+    if (c == 'D') return 2;
+    if (c == 'U') return 3;
+    return -1;
+}
+
+int modpow(int x, int n, int m = MOD) {
+    if (n == 0) return 1%m;
+    int u = modpow(x, n/2, m);
+    u = (u * 1LL * u) % m;
+    if (n%2 == 1) u = (u * 1LL * x) % m;
+    return u;
+}
+int add(int a, int b, int m = MOD) { return (a + b) % m; }
+int sub(int a, int b, int m = MOD) { return (a - b + m) % m; }
+int mul(int a, int b, int m = MOD) { return (a * 1LL * b) % m; }
+int modinv(int a, int m = MOD) { return modpow(a, m-2, m); }
+int moddiv(int a, int b, int m = MOD) { return mul(a, modinv(b,m), m); }
+int norm(int x, int m = MOD) { x %= m; if(x < 0) x += m; return x; }
+
+
+using pii = pair<int, int>;
+
+void solve () 
+{
+    // solve here
+    
+    int n;
+    cin >> n;
+    
+    int neg1 = 0;
+    map<int, int> freq;
+    
+    for (int i = 0; i < n; i++) {
+        int val;
+        cin >> val;
+        if (val == -1) {
+            neg1++;
+        } else {
+            freq[val]++;
+        }
+    }
+    
+    int w_e = (neg1 > 0) ? modpow(2, neg1 - 1) : 1;
+    int w_o = (neg1 > 0) ? modpow(2, neg1 - 1) : 0;
+    
+    int m = 1;
+    for (auto x : freq) {
+        m = (m * modpow(2, x.second - 1)) % MOD;
+    }
+    
+    int k = 0;
+    for (auto [val, count] : freq) { 
+        if (freq.count(val + 1)) {
+            k++;
+        }
+    }
+    
+    int ans = (w_e + (k % MOD * w_o) % MOD) % MOD;
+    ans = (ans * m) % MOD;
+    
+    cout << ans << endl ;
+
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+   // freopen("input.txt", "r", stdin);   // debug
+
+    // init_fact(); // Uncomment if nCr needed
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve() ; 
+    }
+
+    return 0;
+}
+
