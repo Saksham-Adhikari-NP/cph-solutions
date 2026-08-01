99b4ec56c3843861f097d1f1daf7de2523b0cb01
Saksham-Adhikari-NP <saksham71032@gmail.com>
2026-08-01 22:10:03 +0545

    Auto-commit: Pro_Exam_Eligibility.cpp

---
+++ Pro_Exam_Eligibility.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+//ordered set; 
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
+
+#define int long long
+#define vi vector<int>  
+#define all(a) (a).begin(), (a).end() 
+#define rall(a) (a).rbegin(), (a).rend()
+#define Max(x) (*max_element(all(x)))
+#define Min(x) (*min_element(all(x)))
+#define sz(x) ((int)x.size())
+#define Unique(x) sort(all(x)); (x).erase(unique(all(x)),x.end())
+
+// bitmanip shortcuts 
+int hsetbit (int a ) { return (63 - __builtin_clzll(a)) ; } 
+int lsetbit (int n ) { return (n & -n) ; }
+int setbit (int n) { return __builtin_popcountll(n); } 
+
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+
+const int MAXN = 2e5+5;
+const int INF = 1e18;
+const int MOD = 1e9+7; 
+
+
+// vector io
+template<typename T>
+istream& operator >> (istream& s, vector<T>& v){ for(auto &x: v) s >> x; return s; }
+template<typename T>
+ostream& operator << (ostream& s, const vector<T>& v){ for(auto &x: v) s << x << ' '; return s; }
+
+inline void printYN(bool t) { cout << (t ? "YES" : "NO" ) << endl; }
+
+struct Edge {
+    int to;
+    long long weight;
+};
+
+using Graph = std::vector<std::vector<int>>;
+using WeightedGraph = std::vector<std::vector<Edge>>;
+
+ int dx[]={0,0,1,-1};
+ int dy[]={1,-1,0,0};
+ string ds="RLDU";
+int get_dir_idx(char c) {
+    if (c == 'R') return 0;
+    if (c == 'L') return 1;
+    if (c == 'D') return 2;
+    if (c == 'U') return 3;
+    return -1;
+}
+
+using pii = pair<int, int>;
+bool check(double x, int n, int k, const string s) {
+
+    vector<double> remx(n + 1, 0.0);
+    vector<int> wins(n + 1, 0);
+    
+    for(int i = 0; i < n; i++) {
+        remx[i+1] = remx[i] + ((s[i] == 'o' ? 1.0 : 0.0) - x);
+        wins[i+1] = wins[i] + (s[i] == 'o' ? 1 : 0);
+    }
+    
+    double min_remx = INF ; 
+    int valid_idx = 0;
+    
+    for(int r = 1; r <= n; r++) {
+        while (valid_idx <= r && wins[r] - wins[valid_idx] >= k) {
+            min_remx = min(min_remx, remx[valid_idx]);
+            valid_idx++;
+        }
+
+        if (valid_idx > 0 && remx[r] - min_remx >= 0.0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void solve () 
+{
+    // solve here
+    int n, k; 
+    cin >> n >> k;
+    string s; 
+    cin >> s;
+    
+    double low = 0.0, high = 1.0;
+
+   for(int i = 0; i < 49; i++) {
+        double mid = low + (high - low) / 2.0;
+        if (check(mid, n, k, s)) low = mid;
+        else  high = mid; 
+        
+    }
+    
+    cout << fixed << setprecision(11) << high << "\n";
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+   // freopen("input.txt", "r", stdin);   // debug
+
+    // init_fact(); // Uncomment if nCr needed
+        solve() ; 
+        return 0;
+}
+
