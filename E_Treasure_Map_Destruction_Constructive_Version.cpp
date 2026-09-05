381db109e5d9ae73f82932fa102626a2ffeb1d1c
Saksham-Adhikari-NP <saksham71032@gmail.com>
2026-09-06 00:20:44 +0545

    Auto-commit: E_Treasure_Map_Destruction_Constructive_Version.cpp

---
+++ E_Treasure_Map_Destruction_Constructive_Version.cpp
@@ -0,0 +1,136 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+//ordered set; 
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
+
+#define int long long
+#define vi vector<int>  
+#define all(a) (a).begin(), (a).end() 
+#define rall(a) (a).rbegin(), (a).rend()
+#define Max(x) (*max_element(all(x)))
+#define Min(x) (*min_element(all(x)))
+#define sz(x) ((int)x.size())
+#define Unique(x) sort(all(x)); (x).erase(unique(all(x)),x.end())
+
+// bitmanip shortcuts 
+int hsetbit (int a ) { return (63 - __builtin_clzll(a)) ; } 
+int lsetbit (int n ) { return (n & -n) ; }
+int setbit (int n) { return __builtin_popcountll(n); } 
+
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+
+const int MAXN = 2e5+5;
+const int INF = 1e18;
+const int MOD = 1e9+7; 
+
+
+// vector io
+template<typename T>
+istream& operator >> (istream& s, vector<T>& v){ for(auto &x: v) s >> x; return s; }
+template<typename T>
+ostream& operator << (ostream& s, const vector<T>& v){ for(auto &x: v) s << x << ' '; return s; }
+
+inline void printYN(bool t) { cout << (t ? "YES" : "NO" ) << endl; }
+
+struct Edge {
+    int to;
+    long long weight;
+};
+
+using Graph = std::vector<std::vector<int>>;
+using WeightedGraph = std::vector<std::vector<Edge>>;
+
+ int dx[]={0,0,1,-1};
+ int dy[]={1,-1,0,0};
+ string ds="RLDU";
+int get_dir_idx(char c) {
+    if (c == 'R') return 0;
+    if (c == 'L') return 1;
+    if (c == 'D') return 2;
+    if (c == 'U') return 3;
+    return -1;
+}
+
+using pii = pair<int, int>;
+
+void solve () 
+{
+    // solve here
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    cin >> arr;
+
+    vector<int> d(n + 1, 0);
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] > 0) {
+            int l = max(0ll, i - arr[i] + 1);
+            int r = min(n - 1, i + arr[i] - 1);
+            d[l]++;
+            d[r + 1]--;
+        }
+    }
+
+    vector<int> bad(n, 0);
+    int acc = 0;
+    bool has_free = false;
+    for (int i = 0; i < n; ++i) {
+        acc += d[i];
+        if (acc > 0) {
+            bad[i] = 1;
+        } 
+        else has_free = true ; 
+    }
+
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] == 0 && bad[i]) {
+            cout << -1 << endl;
+            return;
+        }
+    }
+
+    if (!has_free) {cout << -1 << endl ; return ; }
+
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] > 0) {
+            bool l = (i - arr[i] >= 0) && !bad[i - arr[i]];
+            bool r = (i + arr[i] < n) && !bad[i + arr[i]];
+            if (!l && !r) {
+                cout << -1 << endl;
+                return;
+            }
+        }
+    }
+
+    vector<char>ans(n) ; 
+    for (int i = 0; i < n; ++i) {
+        ans[i]= (bad[i] ? '0' : '1');
+    }
+    for(auto x:ans) cout <<x ; 
+    cout <<endl ; 
+    
+
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+   // freopen("input.txt", "r", stdin);   // debug
+
+    // init_fact(); // Uncomment if nCr needed
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve() ; 
+    }
+
+    return 0;
+}
+
